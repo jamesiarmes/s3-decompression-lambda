75db51a62b31a364cc35e2575a28b72935369225
James I. Armes <jamesiarmes@gmail.com>
2020-11-24 21:45:18 -0500

    Cleanup.

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,39 +1,30 @@
-require 'logger'
-require 'json'
-# require 'aws-sdk-lambda'
 require 'aws-sdk-s3'
-require 'zlib'
-require 'stringio'
-
-# $client = Aws::Lambda::Client.new()
-# $client.get_account_settings()
-$s3 = Aws::S3::Client.new()
-
 require 'aws-xray-sdk/lambda'
+require 'logger'
+require 'zlib'
+
+$s3 = Aws::S3::Client.new()
 
 def lambda_handler(event:, context:)
   logger = Logger.new($stdout)
-  # logger.info('## ENVIRONMENT VARIABLES')
-  # vars = Hash.new
-  # ENV.each do |variable|
-  #   vars[variable[0]] = variable[1]
-  # end
-  # logger.info(vars.to_json)
-  # logger.info('## EVENT')
-  # logger.info(event.to_json)
-  # logger.info('## CONTEXT')
-  # logger.info(context)
-  # $client.get_account_settings().account_usage.to_h
 
+  destination_bucket = ENV.fetch('DESTINATION_BUCKET', 'armesnet')
+  logger.info("Destination bucket: #{destination_bucket}")
+
+  # Get the object location.
   bucket = event['Records'][0]['s3']['bucket']['name']
   key = event['Records'][0]['s3']['object']['key']
   logger.info("bucket: #{bucket} key: #{key}")
 
-  obj = $s3.get_object(bucket: bucket, key: key)
-  uncompressed = Zlib::GzipReader.new(Zlib::GzipReader.new(obj.body))
+  # CloudWatch Logs get double compressed, so we need to unzip the contents
+  # twice.
+  data = $s3.get_object(bucket: bucket, key: key)
+  uncompressed = Zlib::GzipReader.new(Zlib::GzipReader.new(data.body))
 
-  destination = "#{File.dirname(key)}/#{File.basename(key, '.gz')}"
+  # Update the file extension before writing.
+  destination = "#{File.dirname(key)}/#{File.basename(key, '.gz')}.json"
   logger.info("Destination key: #{destination}")
 
-  $s3.put_object(bucket: 'armesnet', body: uncompressed.read, key: destination)
+  $s3.put_object(bucket: destination_bucket, body: uncompressed.read,
+                 key: destination)
 end
